42f9be441b03db29f4515e5e78c67dd6bde239af
Tino Muzambi <tinomuzambi@gmail.com>
2021-11-11 04:19:55 +0200

    Adds return types.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
-const Home = () => (
-	<main>
-		<p>Clean Next.js with TypeScript and Sass Boilerplate</p>
-	</main>
-);
+const Home: React.FC = (): JSX.Element => {
+	return (
+		<main>
+			<p>Clean Next.js with TypeScript and Sass Boilerplate</p>
+		</main>
+	);
+};
 
 export default Home;
