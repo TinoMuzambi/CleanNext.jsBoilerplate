2167cd655e26794a79b2ed01d8d9d941dd0edca9
Tino Muzambi <tinomuzambi@gmail.com>
2022-01-12 17:35:44 +0200

    Removes types.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,14 +1,13 @@
-import { AppProps } from "next/app";
 import { useEffect } from "react";
 
 import Wrapper from "../components/Wrapper";
 import "../sass/App.scss";
 
-const MyApp: Function = ({ Component, pageProps }: AppProps) => {
+const MyApp = ({ Component, pageProps }) => {
 	useEffect(() => {
-		const disableReactDevTools = (): void => {
-			const noop = (): void => undefined;
-			const DEV_TOOLS = (window as any).__REACT_DEVTOOLS_GLOBAL_HOOK__;
+		const disableReactDevTools = () => {
+			const noop = () => undefined;
+			const DEV_TOOLS = window.__REACT_DEVTOOLS_GLOBAL_HOOK__;
 
 			if (typeof DEV_TOOLS === "object") {
 				for (const [key, value] of Object.entries(DEV_TOOLS)) {
