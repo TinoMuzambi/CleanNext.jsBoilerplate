181fea504980339cebc8d2c32fe5d10f52536b63
Tino Muzambi <tinomuzambi@gmail.com>
2022-01-13 09:56:14 +0200

    Removes code.

--- pages/api/post/index.js
+++
@@ -1,5 +0,0 @@
-const handler = (req, res) => {
-	res.status(200).json({ message: "Hello there" });
-};
-
-export default handler;
--- pages/index.jsx
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-	return (
-		<main>
-			<p>Clean Next.js with TypeScript and Sass Boilerplate</p>
-		</main>
-	);
-};
-
-export default Home;
