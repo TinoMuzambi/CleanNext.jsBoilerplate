090d4cabcb67733048e5845546bbf0470c3d33bf
Tino Muzambi <tinomuzambi@gmail.com>
2022-07-04 15:13:37 +0200

    Adds context.

--- components/Wrapper.jsx
+++ components/Wrapper.jsx
@@ -1,11 +1,12 @@
+import { AppProvider } from "../context/AppContext";
 import Meta from "./Meta";
 
 const Wrapper = ({ children }) => {
 	return (
-		<>
+		<AppProvider>
 			<Meta />
 			{children}
-		</>
+		</AppProvider>
 	);
 };
 
